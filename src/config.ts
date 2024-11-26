4ba54c9a30978c6015bd9916e9a70a232112e02b
Olamilekan840 <Olamilekanabdulazeez840@gmail.com>
2024-11-26 12:44:46 +0000

    Update config.ts
    meta description

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Abdulazeez|Digital Marketer';
-export const SITE_DESCRIPTION = 'Am a professional of digital marketer who leverages digital channels and tools to drive brand awareness,engagement conversion.';
+export const SITE_DESCRIPTION = 'Am a professional of digital marketer who leverages digital channels and tools to drive brand.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
