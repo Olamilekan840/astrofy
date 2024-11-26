078e03ede03fa82adb17f30a03c0d63693b024bf
Olamilekan840 <Olamilekanabdulazeez840@gmail.com>
2024-11-26 12:52:46 +0000

    Update config.ts
    meta tittle

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Abdulazeez|Digital Marketer Expert';
+export const SITE_TITLE = 'Abdulazeez Olamilekan |Digital Marketer Expert';
 export const SITE_DESCRIPTION = 'Am a professional of digital marketer who leverages digital channels and tools to drive brand.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
